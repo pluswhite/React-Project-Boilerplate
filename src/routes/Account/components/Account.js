0028604a30fb5bfd7726a261d62c1292edba6404
Eric.zhang <beefjava@gmail.com>
2017-04-19 19:37:19 +0800

    modify login & account page

--- src/routes/Account/components/Account.js
+++ src/routes/Account/components/Account.js
@@ -69,48 +69,46 @@ class Account extends Component {
 		let tokenExpiryDate = exp.toUTCString()
     // console.log(this.props)
 		return (
-      <div>
+      <div className="container">
         <h2>Account</h2>
-        <div className="container">
-          <div className="col-md-6">
-            <form className="form-horizontal">
-              <div className="col-sm-4 col-xs-12">
-                <div className="form-group">
-                  <label htmlFor="profileImage" className="control-label">Avatar</label>
-                </div>
-                <img src={picture || defaultPicture} style={{height: '5rem'}} />
+        <div className="col-md-6">
+          <form className="form-horizontal">
+            <div className="col-sm-4 col-xs-12">
+              <div className="form-group">
+                <label htmlFor="profileImage" className="control-label">Avatar</label>
               </div>
-              <div className="col-sm-8 col-xs-12">
-                <div className="form-group">
-                  <label htmlFor="profileUsername" className="control-label">Name</label>
-                  <input type="text" disabled placeholder="Display name" id="profileUsername" className="form-control" value={this.state.displayName} onChange={this.onChange('displayName')} />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="profileEmail" className="control-label">Email</label>
-                  <input type="email" disabled placeholder="Email" id="profileEmail" className="form-control" value={this.state.email} onChange={this.onChange('email')} />
-                </div>
-              </div>
-              <div className="col-xs-12">
-                <div className="form-group">
-                  <label htmlFor="profileBio" className="control-label">Bio</label>
-                  <textarea cols="30" rows="10" placeholder="Bio" id="profileBio" className="form-control" style={{border: '1px solid #ccc'}} value={this.state.bio} onChange={this.onChange('bio')}></textarea>
-                  <div className="pull-right help-block">{this.state.bioCharsLeft}</div>
-                </div>
-                <button type="button" className="pull-right btn btn-primary" onClick={this.onSubmit}>UpdateInformation</button>
-              </div>
-            </form>
-          </div>
-          <div className="col-md-6">
-            <h3>Refresh Token</h3>
-            <div className="">
-              <button type="button" className="btn-xs pull-right btn btn-default" onClick={this.onRefresh}>Refresh</button>
+              <img src={picture || defaultPicture} style={{height: '5rem'}} />
             </div>
-            <div className="panel panel-default">
-              <div className="panel-body">
-                <strong>{tokenExpiryDate}</strong>
-                <div style={{width: '100%', marginBottom: '10px', overflowX: 'scroll'}}>
-                  <textarea style={{width: '100%', marginBottom: '10px', overflowX: 'scroll'}} rows="3" readOnly value={token.raw}></textarea>
-                </div>
+            <div className="col-sm-8 col-xs-12">
+              <div className="form-group">
+                <label htmlFor="profileUsername" className="control-label">Name</label>
+                <input type="text" disabled placeholder="Display name" id="profileUsername" className="form-control" value={this.state.displayName} onChange={this.onChange('displayName')} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="profileEmail" className="control-label">Email</label>
+                <input type="email" disabled placeholder="Email" id="profileEmail" className="form-control" value={this.state.email} onChange={this.onChange('email')} />
+              </div>
+            </div>
+            <div className="col-xs-12">
+              <div className="form-group">
+                <label htmlFor="profileBio" className="control-label">Bio</label>
+                <textarea cols="30" rows="10" placeholder="Bio" id="profileBio" className="form-control" style={{border: '1px solid #ccc'}} value={this.state.bio} onChange={this.onChange('bio')}></textarea>
+                <div className="pull-right help-block">{this.state.bioCharsLeft}</div>
+              </div>
+              <button type="button" className="pull-right btn btn-primary" onClick={this.onSubmit}>UpdateInformation</button>
+            </div>
+          </form>
+        </div>
+        <div className="col-md-6">
+          <h3>Refresh Token</h3>
+          <div className="">
+            <button type="button" className="btn-xs pull-right btn btn-default" onClick={this.onRefresh}>Refresh</button>
+          </div>
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <strong>{tokenExpiryDate}</strong>
+              <div style={{width: '100%', marginBottom: '10px', overflowX: 'scroll'}}>
+                <textarea style={{width: '100%', marginBottom: '10px', overflowX: 'scroll'}} rows="3" readOnly value={token.raw}></textarea>
               </div>
             </div>
           </div>
--- src/routes/Login/components/login.js
+++ src/routes/Login/components/login.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
-import { Grid, Col, Panel } from 'react-bootstrap'
-import { Form, FormGroup, FormControl } from 'react-bootstrap'
-import { Checkbox, Button, ControlLabel, HelpBlock } from 'react-bootstrap'
 import { Link } from 'react-router'
 
 import { validateEmail, validatePassword } from './user-validators'
 
 import './login.scss'
-
-const css = {
-	signupBlock: { textAlign: 'center', marginTop: '1em' },
-	signupSpan: { paddingRight: '0.5em' },
-	panelTitle: { fontSize: '.5rem' }
-}
 
 class Login extends Component {
   static propTypes = {
@@ -81,74 +71,34 @@ class Login extends Component {
 		);
 
 		return (
-			<Grid>
-				<Col md={8}>
-					<Panel><Col xs={10} xsPush={1}>
-            <h2 style={css.panelTitle}>Login with email</h2>
-						<Form horizontal>
-							<FormGroup controlId="loginEmail" validationState={this.getValidationState('email')}>
-								<Col componentClass={ControlLabel} sm={2}>
-									<ControlLabel>
-										Email
-									</ControlLabel>
-								</Col>
-								<Col sm={10}>
-									<FormControl type="email" placeholder="Email"
-										value={email.value}
-										onChange={this.onChange('email')}
-									/>
-									<FormControl.Feedback />
-									<HelpBlock>
-										{email.error}
-									</HelpBlock>
-								</Col>
-							</FormGroup>
-							<FormGroup controlId="loginPassword" validationState={this.getValidationState('password')}>
-								<Col componentClass={ControlLabel} sm={2}>
-									Password
-								</Col>
-								<Col sm={10}>
-									<FormControl type="password" placeholder="Password"
-										value={password.value}
-										onChange={this.onChange('password')}
-									/>
-									<FormControl.Feedback />
-									<HelpBlock>
-										{password.error}
-									</HelpBlock>
-								</Col>
-							</FormGroup>
-
-							<Button bsStyle="primary" block
-								disabled={!formIsReady}
-								onClick={this.onLoginClick}>
-								Log in
-							</Button>
-						</Form>
-						<HelpBlock style={css.signupBlock}>
-								<span style={css.signupSpan}>
-									Don't have an account yet?
-								</span>
-								<Link to="/register">Register</Link>
-						</HelpBlock>
-					</Col></Panel>
-				</Col>
-			</Grid>
+			<div className="container">
+        <h2>Login with email</h2>
+        <form className="form-horizontal">
+          <div className="form-group has-feedback has-success">
+            <label htmlFor="loginEmail" className="control-label">Email</label>
+            <div className="col-sm-10">
+              <input type="email" placeholder="Email" id="loginEmail" className="form-control" value={email.value} onChange={this.onChange('email')} />
+              <span className="form-control-feedback glyphicon glyphicon-ok"></span>
+              <div className="help-block">{email.error}</div>
+            </div>
+          </div>
+          <div className="form-group has-feedback">
+            <label htmlFor="loginPassword" className="col-sm-2 control-label">Password</label>
+            <div className="col-sm-10">
+              <input type="password" placeholder="Password" id="loginPassword" className="form-control" value={password.value} onChange={this.onChange('password')} />
+              <span className="form-control-feedback glyphicon glyphicon-ok"></span>
+              <div className="help-block">{password.error}</div>
+            </div>
+          </div>
+          <button className="btn btn-primary btn-block" disabled={!formIsReady} onClick={this.onLoginClick} type="button">Log in</button>
+        </form>
+        <div className="help-block" style={{textAlign: 'inherit', marginTop: '1em'}}>
+          <span style={{paddingRight: '0.5em'}}>Don't have an account yet?</span>
+          <Link to="/register">Register</Link>
+        </div>
+      </div>
 		)
 	}
-
-  /*render() {
-    // console.log(this.props)
-    const { fetchAuth } = this.props
-    return (
-      <div>
-        <h2>Auth</h2>
-        <button onClick={fetchAuth}>
-          Fetch Auth
-        </button>
-      </div>
-    )
-  }*/
 }
 
 export default Login
