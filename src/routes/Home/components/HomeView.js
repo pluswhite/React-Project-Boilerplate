05f330ad8a865d267f4b973938c085e97d16571b
Thsi <beefjava@gmail.com>
2018-12-22 23:11:34 +0800

    refactor(components): modify function component to class

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
-class HomeView extends Component {
+class HomeView extends PureComponent {
   render () {
     return (
       <div>
