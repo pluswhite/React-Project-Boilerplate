fa640738deda06b7ae0fa98902df8812b1ba0ecf
Eric Thsi <beefjava@gmail.com>
2020-09-29 21:28:09 +0800

    refactor: 💡 remove old project structure

--- src/routes/Counter/index.js
+++
@@ -1,8 +0,0 @@
-import { injectReducer } from '../../store/reducers'
-import Counter from './containers/CounterContainer'
-import reducer from './modules/counter'
-
-export default store => {
-  injectReducer(store, { key: 'counter', reducer })
-  return Counter
-}
