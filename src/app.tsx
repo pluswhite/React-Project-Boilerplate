f9cef46a82afaeb3251759237325e251b4bc3217
Eric Thsi <beefjava@gmail.com>
2020-09-30 21:34:37 +0800

    style: 💄 add stylelint config

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import './styles/index.scss';
 import './styles/index.css';
 
 function App() {
-  return <div>React App.</div>;
+  return <div className="app">React App.</div>;
 }
 
 export default App;
